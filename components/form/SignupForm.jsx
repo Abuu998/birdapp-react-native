3d51487085b41c69ce32a19d25cccea3dcc5e551
Abubakar <abubakarndu@gmail.com>
2024-06-28 07:04:53 +0200

    feat: added form validation in signup form

--- components/form/SignupForm.jsx
+++ components/form/SignupForm.jsx
@@ -4,7 +4,11 @@ import InputText from './Input'
 import Label from './Label'
 import Button from '../Button'
 import Checkbox from '../Checkbox'
+import { signupSchema } from '../../utils/validateForm'
+import { ValidationError } from 'yup'
 import colors from 'tailwindcss/colors'
+import ErrorText from '../text/ErrorText'
+import { registerUser } from '../../utils/fetch'
 
 const initialState = {
     name: '',
@@ -16,6 +20,12 @@ const initialState = {
 export default function SignupForm() {
     const [showPass, setShowPass] = useState(true)
     const [data, setData] = useState(initialState)
+    const [errors, setErrors] = useState({
+        exists: false,
+        error: '',
+        ...initialState,
+    })
+    const [loading, setLoading] = useState(false)
 
     const toggleShowPass = () => {
         setShowPass((prev) => !prev)
@@ -23,6 +33,51 @@ export default function SignupForm() {
 
     const onChangeText = (name, text) => {
         setData((prev) => ({ ...prev, [name]: text }))
+    }
+
+    const onSignup = async () => {
+        setLoading(true)
+        setErrors({ exists: false, error: '', ...initialState })
+        let validatedData
+
+        try {
+            validatedData = await signupSchema.validate(data, {
+                abortEarly: false,
+            })
+        } catch (err) {
+            if (err instanceof ValidationError) {
+                // Extracting Yup specific validation errors from list of total errors
+                const yupErrors = {}
+                err.inner.forEach((innerError) => {
+                    yupErrors[innerError.path] = innerError.message
+                })
+                // Saving extracted errors
+                setErrors({ exists: true, ...yupErrors })
+            }
+
+            ToastAndroid.show('Please enter valid data!', ToastAndroid.SHORT)
+            return
+        } finally {
+            setLoading(false)
+        }
+
+        try {
+            const { success, data, error } = await registerUser(validatedData)
+
+            if (success) {
+                setData(initialState)
+                ToastAndroid.show(data.message, ToastAndroid.SHORT)
+                router.replace('/signin')
+                return
+            } else {
+                ToastAndroid.show(error.message, ToastAndroid.SHORT)
+                setErrors((prev) => ({ ...prev, error: error.message }))
+            }
+        } catch (error) {
+            setErrors(error)
+        } finally {
+            setLoading(false)
+        }
     }
 
     return (
@@ -35,6 +90,9 @@ export default function SignupForm() {
                     value={data.name}
                     autoFocus
                 />
+                {errors.name && (
+                    <ErrorText style="mt-1">{errors.name}</ErrorText>
+                )}
             </Label>
             <Label label="Username">
                 <InputText
@@ -43,6 +101,9 @@ export default function SignupForm() {
                     onChangeText={onChangeText}
                     value={data.username}
                 />
+                {errors.username && (
+                    <ErrorText style="mt-1">{errors.username}</ErrorText>
+                )}
             </Label>
             <Label label="Email">
                 <InputText
@@ -53,6 +114,9 @@ export default function SignupForm() {
                     onChangeText={onChangeText}
                     value={data.email}
                 />
+                {errors.email && (
+                    <ErrorText style="mt-1">{errors.email}</ErrorText>
+                )}
             </Label>
             <Label label="Password">
                 <InputText
@@ -62,6 +126,9 @@ export default function SignupForm() {
                     onChangeText={onChangeText}
                     value={data.password}
                 />
+                {errors.password && (
+                    <ErrorText style="mt-1">{errors.password}</ErrorText>
+                )}
                 <Checkbox
                     color={colors.emerald[600]}
                     label="Show Password"
@@ -73,8 +140,9 @@ export default function SignupForm() {
             <Button
                 btnStyles="py-2 px-4 rounded-md bg-emerald-500 mt-8"
                 textStyle="text-lg font-semibold text-white text-center"
+                onPress={onSignup}
             >
-                Signup
+                {loading ? 'Signing you up...' : 'Signup'}
             </Button>
         </View>
     )
--- utils/fetch.js
+++ utils/fetch.js
@@ -14,3 +14,19 @@ export const loginUser = async (data) => {
         return { success: false, error }
     }
 }
+
+export const registerUser = async (data) => {
+    try {
+        const res = await fetch("http://localhost:3000/api/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+
+        return { success: true, data: await res.json() }
+    } catch (error) {
+        return { success: false, error }
+    }
+}
